973b3389d350e48283bae0cb2d0ab236ea523d4b
Orest <beckupfalcon@gmail.com>
2021-04-08 16:09:33 +0300

    homework 2

--- homework_1/src/main/java/config/SecondConfig.java
+++ homework_1/src/main/java/config/SecondConfig.java
@@ -1,6 +1,8 @@
 package config;
 
+import beans.beans3.BeanD;
 import beans.beans3.BeanE;
+import beans.beans3.BeanF;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -8,6 +10,6 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan("beans.beans2")
 @ComponentScan(basePackages = {"beans.beans3"}, excludeFilters = {
-        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = BeanE.class)})
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*Bean[^DF]")})
 public class SecondConfig {
 }
